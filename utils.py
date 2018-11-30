d5f63504996be8e4cb3584ca32a7614f69487404
mihirsathe <m1sathe@ucsd.edu>
2018-11-30 12:51:40 -0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,38 +3,34 @@ import imageio
 from keras.utils import Sequence
 from os import listdir
 from os.path import isfile, join
-import tensorflow as tf
-from skimage.transform import resize
+from skimage.transform import downscale_local_mean
 
 
 def create_VEDAI(PATH_TO_VEHICLES_FOLDER):
     # Takes in the full path to the unzipped "VEHICULES" folder
-    # Returns RGB and Infrared images in (images, x, y, channels) format
+    # Returns list of filenames of both rgb and infrared images
     NUM_FILES = 2536
     MAX_INDEX = 1272
-    X_PIXELS = 1024
-    Y_PIXELS = 1024
-
     onlyfiles = [f for f in listdir(PATH_TO_VEHICLES_FOLDER) if isfile(
         join(PATH_TO_VEHICLES_FOLDER, f)) and "png" in f]
     assert len(onlyfiles) == NUM_FILES, "Not the full VEDAI 1024 Dataset"
-    rgb = np.zeros((MAX_INDEX, X_PIXELS, Y_PIXELS, 3))
-    infra = np.zeros((MAX_INDEX, X_PIXELS, Y_PIXELS, 1))
+    rgb = []
+    infra = []
     indices = [format(n, '08') for n in range(MAX_INDEX)]
+    missing_offset = 0
     for index in indices:
         pair = [file for file in onlyfiles if str(index) in file]
         if pair:
             for file in pair:
-                im = imageio.imread(file)
                 if "co" in file:
-                    rgb[int(index), :, :, :] = np.reshape(
-                        im, (tuple([1]) + im.shape))
+                    rgb[int(index) - missing_offset] = file
                 elif "ir" in file:
-                    infra[int(index), :, :, :] = np.reshape(
-                        im, (tuple([1]) + im.shape + tuple([1])))
+                    infra[int(index) - missing_offset] = file
         else:
             print("The following image is missing!: " + index)
-
+            missing_offset += 1
+    assert len(rgb) == len(infra), "Not every file has its pair!"
+    assert len(rgb) == NUM_FILES / 2, "Didn't save every image."
     return rgb, infra
 
 
@@ -64,46 +60,33 @@ def combine_rgb_infra(rgb, infra):
     return four_channel
 
 
-def overlapping_patches(images, patch_size=(64, 64), padding="VALID"):
-    sess = tf.Session()
-
-    num_images, size_x, size_y, channels = images.shape
-    ims = tf.convert_to_tensor(images)
-    patch_x, patch_y = patch_size
-    patches = tf.extract_image_patches(ims, [1, patch_x, patch_y, 1], [
-        1, patch_x, patch_y, 1], [1, 1, 1, 1], padding=padding)
-    patches_shape = tf.shape(patches)
-    with sess.as_default():
-        np = tf.reshape(patches, [tf.reduce_prod(patches_shape[0:3]),
-                                  patch_x, patch_y, channels]).eval()
-        return np
-
-
 def non_overlapping_patches(image, patch_size=(64, 64)):
     size_x, size_y, channels = image.shape
     patch_x, patch_y = patch_size
-    im_pad = np.pad(image, ((0, size_x % patch_x),
-                            (0, size_y % patch_y), (0, 0)), mode="constant")
-    num_patches = (size_x // patch_x + 1) * (size_y // patch_y + 1)
-    patches = np.zeros(num_patches, patch_x, patch_y, channels)
+    im_pad = np.pad(image, ((0, patch_x - size_x % patch_x),
+                            (0, patch_y - size_y % patch_y), (0, 0)),
+                    mode="constant")
+    if size_x % patch_x == 0 and size_y % patch_y == 0:
+        im_pad = image
+    pad_x, pad_y, channels = im_pad.shape
+    print(im_pad.shape)
+    num_patches = (pad_x // patch_x) * (pad_y // patch_y)
+    patches = np.zeros((num_patches, patch_x, patch_y, channels))
     counter = 0
-    for i in range((size_x // patch_x) + 1):
-        for j in range((size_y // patch_y) + 1):
+    for i in range((pad_x // patch_x)):
+        for j in range((pad_y // patch_y)):
             x_s = i * patch_x
             y_s = j * patch_y
-            patches[counter, :, :, :] = im_pad[x_s:x_s + patch_x - 1,
-                                               y_s:y_s + patch_y - 1, :]
+            patches[counter, :, :, :] = im_pad[x_s:x_s + patch_x,
+                                               y_s:y_s + patch_y, :]
             counter += 1
     return patches
 
 
-def downsample_image(image, factor=4):
+def downsample_image(image, block=[2, 2]):
     # Downsamples numpy array image by factor
     # Returns the image and the downsampled copy in a tuple
-    h, w = image.size
-    h = h // factor
-    w = w // factor
-    return image, resize(image, (h, w))
+    return image, downscale_local_mean(image, block)
 
 
 def reconstruct_patches(patches, image_size):
@@ -115,18 +98,30 @@ def reconstruct_patches(patches, image_size):
 
 class VEDAISequence(Sequence):
 
-    def __init__(self, x_set, y_set, batch_size, augmentations):
-        # TODO: Initialize keras augmentor, save self variables
-        pass
+    def __init__(self, rgb, infra, ims_per_batch):
+        # TODO: Initialize keras augmentor
+        self.r, self.i = rgb, infra
+        self.batch_size = ims_per_batch
 
     def __len__(self):
-        # TODO: Return number of batches given data and augmentations
-        pass
+        return int(np.ceil(len(self.r) / float(self.batch_size)))
 
     def __getitem__(self, idx):
-        # TODO: Return a batch of correct size from augmentor
-        pass
-
-    def on_epoch_end(self):
-        # TODO: Reseed the augmentor with new augmentations
-        pass
+        batchsz = 256 * self.batch_size
+        channels = 4
+        patch_x, patch_y = 64, 64
+        start = idx * self.batch_size
+        end = (idx + 1) * self.batch_size
+        high_res = np.zeros((batchsz, patch_x, patch_y, channels))
+        low_res = np.zeros((batchsz, patch_x // 2, patch_y // 2, channels))
+        im_num = 0
+        for ind in range(start, end):
+            st, stp = im_num * 256, (im_num + 1) * 256
+            im_num += 1
+            rgb = imageio.imread(self.r[ind])
+            infra = imageio.imread(self.i[ind])
+            high_res[st:stp, :, :, :] = non_overlapping_patches(
+                combine_rgb_infra(rgb, infra))
+        for hr in range(batchsz):
+            low_res[hr, :, :, :] = downsample_image(high_res)
+        return low_res, high_res
